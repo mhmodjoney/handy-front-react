aaeb35bdc3ccdb6ae81f9870befb820b739e29be
Mahmoud Jouny <mahmoudjouny99@gmail.com>
2023-08-24 14:59:24 +0300

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -181,7 +181,7 @@ export default function SignUp() {
               error={PasswordError ? true : false}
               helperText={
                 PasswordError
-                  ? "Password must be at least 8 letters and numbers"
+                  ? "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one symbo"
                   : ""
               }
               type={showPassword ? "text" : "password"}
